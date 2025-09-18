cc27ee30cfc971eeea9db8fec487409a609f5b99
__stdcall <hanghang0702@hotmail.com>
2025-09-18 10:48:39 +0800

    fix configure.sh stuck

--- configure.sh
+++ configure.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-conda activate /home/featurize/work/mch/env
+if ! conda activate /home/featurize/work/mch/env 2>/dev/null; then
+    echo "Run with 'source ./configure.sh'" >&2
+    exit 1
+fi
+
 pip install --user -r requirements.txt
 pip install --user -r requirements_demo.txt
 pip install --user "httpx[socks]"
 
 pip install --user ninja
-pip install --user --no-build-isolation --verbose flash-attn
+MAX_JOBS=4 pip install --user --no-build-isolation --verbose flash-attn
 
 featurize port export 7860
